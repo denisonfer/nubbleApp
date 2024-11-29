af0c68279ce709bd29fb78f11d0753bbd925b967
denisonfer <denisonferreiram91@gmail.com>
2024-11-29 08:51:46 -0300

    feat: added delay in get post list api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,13 @@
+import { Platform } from 'react-native';
+
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:3333',
+  baseURL: Platform.select({
+    ios: 'http://localhost:3333',
+    android: 'http://10.0.2.2:3333',
+  }),
   headers: {
-    Authorization: `Bearer Mw.NMcPB9-4NDpy59qhK1k6RYi9ZY-ouFV9IT92yPjECBbRn-BAt2btcDpWzAS0`,
+    Authorization: `Bearer Mw.t7ev3KeeJGdHQpbFOOG5Ol_V9kg7XHHhBevnwAJU2Dy85o8mNfNkHTzTjVFI`,
   },
 });
